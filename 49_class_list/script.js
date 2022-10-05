d170d60f6703924d60de98c7e6fa56443a2c73d4
Samuel Candido <92275912+Muka1993@users.noreply.github.com>
2022-10-04 21:46:12 -0300

    Nova atividade

---
+++ 49_class_list/script.js
@@ -0,0 +1,21 @@
+
+
+// let red = document.querySelector('.red');
+
+// red.classList.remove('red');
+
+// console.log(red)
+
+
+function removeClass(){
+    let blue = document.querySelector('#cubo');
+    blue.classList.remove('blue');
+}
+
+function addClass(){
+    let blue = document.querySelector('#cubo');
+    blue.classList.add('blue');
+    
+
+}
+
