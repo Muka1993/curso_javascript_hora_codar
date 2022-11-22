7274ca03c860d4bbc04ba8d3e8d0cfc238e1dbc1
Samuel Candido <92275912+Muka1993@users.noreply.github.com>
2022-11-22 11:30:21 -0300

    Atualizando

--- 51_evento_keypress/script.js
+++ 51_evento_keypress/script.js
@@ -1,9 +1,15 @@
 // KEYPRESS assim que a tecla é pressionada será disparado o evento
 
+const music = new Audio('water-droplets-small-stone.mp3');
+
+
 let text = document.getElementById('text');
 const teclaEnter = 13;
 
 text.addEventListener('keypress', function(evento) {
     if (evento.keyCode == teclaEnter)
     result.style.display = 'block';
-});
+    let inpu = document.querySelector('text');
+    
+} 
+);
